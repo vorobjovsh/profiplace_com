daeb65e33a26ce5f0cf25c1a8775b2a2b428147b
vorobjovsh <vorobjov75@gmail.com>
2022-01-06 09:06:42 +0200

    create app with 320px and 360px

---
+++ src/js/functions/year.function.js
@@ -0,0 +1,4 @@
+function yearInCopiright (id_span) {
+  let yearCopiright = new Date().getFullYear();
+  document.getElementById(id_span).innerText = yearCopiright;
+}
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+/**
+  * название функции
+  *
+  * @param {number} first - первое число
+  * @returns {number}
+  */
+
+ yearInCopiright ("footer-copyright__year");
